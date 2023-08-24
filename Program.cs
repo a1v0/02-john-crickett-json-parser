9402767b65d8379c86a85d35d72c52e13b5d9d98
a1v0 <avc285@gmail.com>
2023-08-24 21:37:05 +0100

    Create plan

--- Program.cs
+++ Program.cs
@@ -2,6 +2,25 @@
 
 Console.WriteLine(JSONParser("ghfksjh"));
 
+// trim input
+// early return if first character isn't "{"
+// create test suite (in no particular order):
+// - returns empty object if string is "{}"
+// - ignores white space and line breaks
+// - parses string key/value pairs
+// - parses numbers (incl. floats)
+// - parses boolean values
+// - parses arrays
+//   - parses nested objects/numbers/strings/arrays/booleans
+// - parses objects
+// - parses null type
+// throws errors:
+// - no quotes on key or string value (or single quotes)
+// - incorrectly escaped strings
+// - unrecognised values (including incorrectly cased NULL/TRUE etc.)
+// - missing comma
+// - missing closing bracket
+
 static object JSONParser(string rawJSON)
 {
     return new Object();
