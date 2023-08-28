5df39944f084f698d0d959bdadc410d3217183ae
a1v0 <avc285@gmail.com>
2023-08-28 14:52:09 +0100

    Refactor comments

--- JSONParser.cs
+++ JSONParser.cs
@@ -1,30 +1,29 @@
 ﻿/*
  * Wanted to make a test suite with these tests but couldn't get xUnit to work:
-* - returns empty object if string is "{}"
-* - ignores white space and line breaks
-* - parses string key/value pairs
-* - parses numbers (incl. floats)
-* - parses boolean values
-* - parses arrays
-* - parses nested objects/numbers/strings/arrays/booleans
-* - parses objects
-* - parses null type
-* - throws errors:
-*   - no quotes on key or string value (or single quotes)
-*   - incorrectly escaped strings
-*   - unrecognised values (including incorrectly cased NULL/TRUE etc.)
-*   - missing comma
-*   - missing closing bracket
- * 
+ * - returns empty object if string is "{}"
+ * - ignores white space and line breaks
+ * - parses string key/value pairs
+ * - parses numbers (incl. floats)
+ * - parses boolean values
+ * - parses arrays
+ * - parses nested objects/numbers/strings/arrays/booleans
+ * - parses objects
+ * - parses null type
+ * - throws errors:
+ *   - no quotes on key or string value (or single quotes)
+ *   - incorrectly escaped strings
+ *   - unrecognised values (including incorrectly cased NULL/TRUE etc.)
+ *   - missing comma
+ *   - missing closing bracket
  */
 
 
 static object ParseJSON(string rawJSON)
 {
-// trim input
-// early return if first character isn't "{"
-// create test suite (in no particular order):
+    // is there a way to create a more specific return type than just "object"? Something like an interface that doesn't require defining property names
+    // trim input
+    // early return if first character isn't "{"
     return new Object();
 }
 
-static string StringifyJSON(object jsonTarget) { return new String(); }
+static string StringifyJSON(object jsonTarget) { return new String(""); }
