b4a1014f22da428adf0b0306d78c4828e10d338b
a1v0 <avc285@gmail.com>
2023-08-28 14:39:22 +0100

    Move unit test comments

--- JSONParser.cs
+++ JSONParser.cs
@@ -1,28 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+ * Wanted to make a test suite with these tests but couldn't get xUnit to work:
+* - returns empty object if string is "{}"
+* - ignores white space and line breaks
+* - parses string key/value pairs
+* - parses numbers (incl. floats)
+* - parses boolean values
+* - parses arrays
+* - parses nested objects/numbers/strings/arrays/booleans
+* - parses objects
+* - parses null type
+* - throws errors:
+*   - no quotes on key or string value (or single quotes)
+*   - incorrectly escaped strings
+*   - unrecognised values (including incorrectly cased NULL/TRUE etc.)
+*   - missing comma
+*   - missing closing bracket
+ * 
+ */
 
-Console.WriteLine(ParseJSON("ghfksjh"));
-
-// trim input
-// early return if first character isn't "{"
-// create test suite (in no particular order):
-// - returns empty object if string is "{}"
-// - ignores white space and line breaks
-// - parses string key/value pairs
-// - parses numbers (incl. floats)
-// - parses boolean values
-// - parses arrays
-//   - parses nested objects/numbers/strings/arrays/booleans
-// - parses objects
-// - parses null type
-// throws errors:
-// - no quotes on key or string value (or single quotes)
-// - incorrectly escaped strings
-// - unrecognised values (including incorrectly cased NULL/TRUE etc.)
-// - missing comma
-// - missing closing bracket
 
 static object ParseJSON(string rawJSON)
 {
+// trim input
+// early return if first character isn't "{"
+// create test suite (in no particular order):
     return new Object();
 }
 
