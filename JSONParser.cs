b4301cdb8f1bea2f7b1ea891f101e9f570b4b087
a1v0 <avc285@gmail.com>
2023-08-29 20:53:55 +0100

    Refactor with try/catch

--- JSONParser.cs
+++ JSONParser.cs
@@ -10,6 +10,7 @@
  * - parses nested objects/numbers/strings/arrays/booleans
  * - parses objects
  * - parses null type
+ * - ignores quotation marks and brackets when escaped and in a string
  * - throws errors:
  *   - no quotes on key or string value (or single quotes)
  *   - incorrectly escaped strings
@@ -18,19 +19,34 @@
  *   - missing closing bracket
  */
 
+ParseJSON("   ");
 ParseJSON("   {");
+ParseJSON("   {}");
 
 static Dictionary<string, dynamic> ParseJSON(string rawJSON)
 {
-    Exception invalidJSON = new Exception("Input is not valid JSON.");
-    string trimmedJSON = rawJSON.Trim();
+    string errorMessage = "Input is not valid JSON.";
+    Exception invalidJSON = new Exception(errorMessage);
+    try
+    {
+        string trimmedJSON = rawJSON.Trim();
 
-    if (trimmedJSON.Length == 0) throw invalidJSON;
-    if (trimmedJSON[0] is not '{') throw invalidJSON;
+        if (trimmedJSON.Length == 0) throw invalidJSON;
+        if (trimmedJSON[0] is not '{') throw invalidJSON;
 
-    Dictionary<string, dynamic> parsedJSON = new Dictionary<string, dynamic>();
+        Dictionary<string, dynamic> parsedJSON = new Dictionary<string, dynamic>();
 
-    return parsedJSON;
+        // loop through JSON
+        // keep tally of all open curly brackets
+        // keep tally of all open square brackets
+        // keep tally of all open strings
+
+        return parsedJSON;
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine(errorMessage);
+    }
 }
 
-static string StringifyJSON(Dictionary<string,dynamic> jsonTarget) { return new String(""); }
+static string StringifyJSON(Dictionary<string, dynamic> jsonTarget) { return new String(""); }
