24528ca220617c9aa6863a99c532f346eb4b59b1
a1v0 <avc285@gmail.com>
2023-09-02 09:59:10 +0100

    Add detail to comments

--- JSONParser.cs
+++ JSONParser.cs
@@ -4,9 +4,12 @@
  * - returns empty object if string is "{           }"
  * - ignores white space and line breaks
  * - parses string key/value pairs
+ *   - since JSON gets sent as a string, any quotation marks inside will be escaped. Therefore, how do I differentiate between the quotation marks that enclose a key and any ones escaped within that key?
+ *   - check what it looks like when JSON arrives
  * - parses numbers (incl. floats)
  * - parses boolean values
  * - parses arrays
+ *   - what should I parse this as? Always as an untyped collection or rather do I validate that it could work as a typed array and conditionally create an array on that basis?
  * - parses nested objects/numbers/strings/arrays/booleans
  * - parses objects
  * - parses null type
