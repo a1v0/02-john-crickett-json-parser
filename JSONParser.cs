3fed1536a4677388f9d9ef7e1a72b5ce8b357884
a1v0 <avc285@gmail.com>
2023-09-16 21:40:01 +0100

    Extract JSONParser to a new file

--- JSONParser.cs
+++ JSONParser.cs
@@ -1,49 +1,4 @@
-﻿/*
- * Wanted to make a test suite with these tests but couldn't get xUnit to work:
- * - returns empty object if string is "{}"
- * - returns empty object if string is "{           }"
- * - ignores white space and line breaks
- * - parses string key/value pairs
- *   - since JSON gets sent as a string, any quotation marks inside will be escaped. Therefore, how do I differentiate between the quotation marks that enclose a key and any ones escaped within that key?
- *   - check what it looks like when JSON arrives
- * - parses numbers (incl. floats)
- * - parses boolean values
- * - parses arrays
- *   - what should I parse this as? Always as an untyped collection or rather do I validate that it could work as a typed array and conditionally create an array on that basis?
- * - parses nested objects/numbers/strings/arrays/booleans
- * - parses objects
- * - parses null type
- * - ignores quotation marks and brackets when escaped and in a string
- * - throws errors:
- *   - no quotes on key or string value (or single quotes)
- *   - incorrectly escaped strings
- *   - unrecognised values (including incorrectly cased NULL/TRUE etc.)
- *   - missing comma
- *   - missing closing bracket
- */
-
-string divider = "----------------------------------------------------------------------------------------";
-
-// PASS TESTS
-Console.WriteLine(divider);
-new JSONParser("{}").Parse(); // test case: PASS empty JSON object
-
-Console.WriteLine(divider);
-new JSONParser("   {          }    ").Parse(); // test case: PASS ignores whitespace between JSON content
-
-Console.WriteLine(divider);
-new JSONParser("{\"key\": \"value\"}").Parse(); // test case: PASS correctly parses a string key/value pair
-
-Console.WriteLine(divider);
-new JSONParser("{\"key1\": \"value\", \"key2\": \"value\"}").Parse(); // test case: PASS correctly parses multiple string key/value pairs
-
-// FAIL TESTS
-Console.WriteLine(divider);
-new JSONParser("   {").Parse(); // test case: FAIL unclosed bracket
-
-Console.WriteLine(divider);
-new JSONParser("   ").Parse(); // test case: FAIL empty string
-
+﻿using System;
 
 public class JSONParser
 {
@@ -131,11 +86,12 @@ public class JSONParser
     private void ExpectKey() { }
     private void ExpectColon() { }
     private void ExpectValue() { }
-    private void ExpectCommaOrEnd() {
+    private void ExpectCommaOrEnd()
+    {
         // for nested objects to work, I reckon this one will need to know whether it's top-level or not. Should be doable using the CharCounter
-        }
+    }
 
-        private void CloseBrackets(char closingCharacter, Dictionary<char, short> charCounter)
+    private void CloseBrackets(char closingCharacter, Dictionary<char, short> charCounter)
     {
         char? openingCharacter = null;
 
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Wanted to make a test suite with these tests but couldn't get xUnit to work:
+ * - returns empty object if string is "{}"
+ * - returns empty object if string is "{           }"
+ * - ignores white space and line breaks
+ * - parses string key/value pairs
+ *   - since JSON gets sent as a string, any quotation marks inside will be escaped. Therefore, how do I differentiate between the quotation marks that enclose a key and any ones escaped within that key?
+ *   - check what it looks like when JSON arrives
+ * - parses numbers (incl. floats)
+ * - parses boolean values
+ * - parses arrays
+ *   - what should I parse this as? Always as an untyped collection or rather do I validate that it could work as a typed array and conditionally create an array on that basis?
+ * - parses nested objects/numbers/strings/arrays/booleans
+ * - parses objects
+ * - parses null type
+ * - ignores quotation marks and brackets when escaped and in a string
+ * - throws errors:
+ *   - no quotes on key or string value (or single quotes)
+ *   - incorrectly escaped strings
+ *   - unrecognised values (including incorrectly cased NULL/TRUE etc.)
+ *   - missing comma
+ *   - missing closing bracket
+ */
+
+string divider = "----------------------------------------------------------------------------------------";
+
+// PASS TESTS
+Console.WriteLine(divider);
+new JSONParser("{}").Parse(); // test case: PASS empty JSON object
+
+Console.WriteLine(divider);
+new JSONParser("   {          }    ").Parse(); // test case: PASS ignores whitespace between JSON content
+
+Console.WriteLine(divider);
+new JSONParser("{\"key\": \"value\"}").Parse(); // test case: PASS correctly parses a string key/value pair
+
+Console.WriteLine(divider);
+new JSONParser("{\"key1\": \"value\", \"key2\": \"value\"}").Parse(); // test case: PASS correctly parses multiple string key/value pairs
+
+// FAIL TESTS
+Console.WriteLine(divider);
+new JSONParser("   {").Parse(); // test case: FAIL unclosed bracket
+
+Console.WriteLine(divider);
+new JSONParser("   ").Parse(); // test case: FAIL empty string
