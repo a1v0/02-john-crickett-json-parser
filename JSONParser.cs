4935076e10bc874dbe039118078b77210cd42c43
a1v0 <avc285@gmail.com>
2023-09-16 21:37:53 +0100

    Extract JSONStringifier to a new file

--- JSONParser.cs
+++ JSONParser.cs
@@ -244,14 +244,3 @@ public class JSONParser
         }
     }
 }
-
-/***************************************************************************************************************************/
-
-public class JSONStringifier
-{
-    public Dictionary<string, dynamic> input;
-
-    public JSONStringifier(Dictionary<string, dynamic> dataToBeStringified) { input = dataToBeStringified; }
-
-    //public string Stringify() { }
-}
---
+++ JSONStringifier.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class JSONStringifier
+{
+    public Dictionary<string, dynamic> input;
+
+    public JSONStringifier(Dictionary<string, dynamic> dataToBeStringified) { input = dataToBeStringified; }
+
+    //public string Stringify() { }
+}
