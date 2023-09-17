43dc08f9a2ebae48594d5dd6f95572b853c0f492
a1v0 <avc285@gmail.com>
2023-09-17 09:34:52 +0100

    Create CurrentCharIndex property

--- JSONParser.cs
+++ JSONParser.cs
@@ -190,6 +190,8 @@ public class JSONParser
         set;
     }
 
+    private int CurrentCharIndex { get; set; }
+
     private void UpdateCurrentProgramState()
     {
         ++CurrentProgramState;
