4528996f2d128139cb1c3d851cc17064a5eca90e
a1v0 <avc285@gmail.com>
2023-09-20 21:59:52 +0100

    Remove completed items from testing checklist

--- Program.cs
+++ Program.cs
@@ -1,18 +1,9 @@
 ﻿/*
  * Wanted to make a test suite with these tests but couldn't get xUnit to work:
- * - returns empty object if string is "{}"
- * - returns empty object if string is "{           }"
- * - ignores white space and line breaks
- * - parses string key/value pairs
- *   - since JSON gets sent as a string, any quotation marks inside will be escaped. Therefore, how do I differentiate between the quotation marks that enclose a key and any ones escaped within that key?
- *   - check what it looks like when JSON arrives
- * - parses numbers (incl. floats)
- * - parses boolean values
  * - parses arrays
  *   - what should I parse this as? Always as an untyped collection or rather do I validate that it could work as a typed array and conditionally create an array on that basis?
  * - parses nested objects/numbers/strings/arrays/booleans
  * - parses objects
- * - parses null type
  * - ignores quotation marks and brackets when escaped and in a string
  * - throws errors:
  *   - no quotes on key or string value (or single quotes)
